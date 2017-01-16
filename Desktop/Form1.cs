9b54ababfd239b95aa6a0e3372bd76f7d09dc96d
NikolaMilicic <nikola.milicic@estudent.hr>
2017-01-16 16:33:34 +0100

    Added Web and WinForms project

---
+++ Desktop/Form1.cs
@@ -0,0 +1,24 @@
+﻿using Business.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            KorisnikServices korisnikServices = new KorisnikServices();
+            var tmp = korisnikServices.GetAll();
+        }
+    }
+}
