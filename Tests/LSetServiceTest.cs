733d051b4669ab6ea57e28b4afdb11900a80d50c
NMilicic <nikola.mlicic991@gmal.com>
2017-01-28 18:48:12 +0100

    WIP

---
+++ Tests/LSetServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data.Domain;
+
+namespace Tests
+{
+    [TestClass]
+    public class LSetServiceTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
